f9189edf60fd89446378bad39448394ae427595f
eric <15854168262@qq.com>
2023-03-18 01:04:27 +0800

    修复迭代器中当迭代所有元素后，调用next的问题

--- stack-queue/Deque.java
+++ stack-queue/Deque.java
@@ -2,11 +2,11 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
-	
-	private class StackIterator implements Iterator<Item>{
-		
+
+	private class DuqueIterator implements Iterator<Item> {
+
 		private Node<Item> currentNode = headNode;
-		
+
 		@Override
 		public boolean hasNext() {
 			return currentNode != null;
@@ -14,31 +14,38 @@ public class Deque<Item> implements Iterable<Item> {
 
 		@Override
 		public Item next() {
+			if (!hasNext()) {
+				throw new NoSuchElementException();
+			}
 			Item ansItem = currentNode.value;
 			currentNode = currentNode.next;
 			return ansItem;
 		}
-		
+
 		@Override
 		public void remove() {
 			throw new UnsupportedOperationException();
 		}
-		
+
 	}
+
 	private class Node<Item> {
 		public Item value;
 		public Node<Item> next;
-		public Node(){}
-		public Node(Item value, Node<Item> next){
+
+		public Node() {
+		}
+
+		public Node(Item value, Node<Item> next) {
 			this.value = value;
 			this.next = next;
 		}
 	}
-	
+
 	private Node<Item> headNode;
 	private Node<Item> tailNode;
 	private int size;
-	
+
 	public Deque() {
 		headNode = null;
 		tailNode = null;
@@ -47,11 +54,11 @@ public class Deque<Item> implements Iterable<Item> {
 
 	// verify the argument is null or not
 	private void valid(Item value) {
-		if(value == null) {
+		if (value == null) {
 			throw new IllegalArgumentException();
 		}
 	}
-	
+
 	public boolean isEmpty() {
 		return headNode == null;
 	}
@@ -62,11 +69,11 @@ public class Deque<Item> implements Iterable<Item> {
 
 	public void addFirst(Item item) {
 		valid(item);
-		
-		if(headNode == null) {
+
+		if (headNode == null) {
 			headNode = new Node<Item>(item, null);
 			tailNode = headNode;
-		}else {
+		} else {
 			Node<Item> tmpNode = new Node<Item>(item, headNode);
 			headNode = tmpNode;
 		}
@@ -75,11 +82,11 @@ public class Deque<Item> implements Iterable<Item> {
 
 	public void addLast(Item item) {
 		valid(item);
-		
-		if(headNode == null) {
+
+		if (headNode == null) {
 			headNode = new Node<>(item, null);
 			tailNode = headNode;
-		}else {
+		} else {
 			tailNode.next = new Node<>(item, null);
 			tailNode = tailNode.next;
 		}
@@ -87,55 +94,55 @@ public class Deque<Item> implements Iterable<Item> {
 	}
 
 	public Item removeFirst() {
-		if(isEmpty()) {
+		if (isEmpty()) {
 			throw new NoSuchElementException();
 		}
-		Node<Item> ansNode = headNode;
-		if(size == 1) {
+		Item ansItem = headNode.value;
+		if (size == 1) {
 			headNode = null;
 			tailNode = null;
-			return ansNode.value;
+		} else {
+			headNode = headNode.next;
 		}
-		headNode = headNode.next;
 		size--;
-		
-		return ansNode.value;
+		return ansItem;
 	}
 
 	public Item removeLast() {
-		if(isEmpty()) {
+		if (isEmpty()) {
 			throw new NoSuchElementException();
 		}
 		Node<Item> node = headNode;
-		if(size == 1) {
+		Item ansItem = null;
+		if (size == 1) {
 			headNode = null;
 			tailNode = null;
-			return node.value;
+			ansItem = node.value;
+		} else {
+			while (node.next != tailNode) {
+				node = node.next;
+			}
+			ansItem = tailNode.value;
+			node.next = null;
+			tailNode = node;
 		}
-		while(node.next != tailNode) {
-			node = node.next;
-		}
-		Item ansItem = tailNode.value;
-		tailNode = node;
 		size--;
 		return ansItem;
 	}
 
 	public Iterator<Item> iterator() {
-		return new StackIterator();
+		return new DuqueIterator();
 	}
-	
+
 	public static void main(String[] args) {
-		Deque<String> queue = new Deque<>();
-		queue.addFirst("Hello World");
-		queue.addFirst("I");
-		queue.removeFirst();
-		queue.removeFirst();
-		queue.addLast("eric");
-		for (String s : queue) {
+		Deque<Integer> queue = new Deque<>();
+		queue.addFirst(1);
+		queue.isEmpty();
+		queue.removeLast();
+		queue.addFirst(4);
+		queue.removeLast();
+		for (int s : queue) {
 			System.out.println(s);
 		}
 	}
 }
-
-
